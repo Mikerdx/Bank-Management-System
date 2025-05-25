d2813b10fa79ebe5734bea31638d552efcef56f0
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-25 20:41:26 +0300

    button

--- bank.py
+++ bank.py
@@ -9,6 +9,15 @@ class bank():
         scrn_height = self.root.winfo_screenheight()
         
         self.root.geometry(f"{scrn_width}x{scrn_height}+0+0")
+        
+        mainLabel = tk.Label(self.root, text = "Bank Account Management System", font=("Arial",40,"bold"),bg="light green", bd=5, relief="groove")
+        mainLabel.pack(side="top", fill="x")
+        
+        mainFrame = tk.Frame(self.root, bg="light grey", bd=5, relief="ridge")
+        mainFrame.place(x=400, y=90, width=450, height=550)
+        
+        openAcBtn = tk.Button(mainFrame, width=20,text="open account",bg="light blue", bd=3, relief="raised", font=("Arial",20,"bold"))
+        openAcBtn.grid(row=0, column=0, padx=40, pady=50)
  
         
 root=tk.Tk()
