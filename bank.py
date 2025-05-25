07691536ec836f439801d08e5a283eea23808efb
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-25 20:28:37 +0300

    Initial commit

---
+++ bank.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class bank():
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Bank Management")
+        
+        scrn_width = self.root.winfo_screenwidth()
+        scrn_height = self.root.winfo_screenheight()
+        
+        self.root.geometry(f"{scrn_width}x{scrn_height}+0+0")
+ 
+        
+root=tk.Tk()
+obj = bank(root)
+root.mainloop(
