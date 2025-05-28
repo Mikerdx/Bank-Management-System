3360689289f863d4447acaf3d7b137060631ebef
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-28 14:02:18 +0300

    id update

--- lib/models/user_profile.py
+++ lib/models/user_profile.py
@@ -1,5 +1,5 @@
 # lib/models/user_profile.py
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, DateTime, func
 from sqlalchemy.orm import relationship
 from lib.models import Base
 
@@ -11,6 +11,8 @@ class UserProfile(Base):
     email = Column(String, unique=True)
     phone = Column(String)
     address = Column(Text)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
     # Reverse one-to-one relationship
     account = relationship("Account", back_populates="profile", uselist=False)
