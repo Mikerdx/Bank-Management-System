c1857ac127f00c21d188f80e2918b4384105ba83
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-28 13:41:55 +0300

    timestamp

--- lib/models/account_product.py
+++ lib/models/account_product.py
@@ -10,6 +10,9 @@ class AccountProduct(Base):
     product_id = Column(Integer, ForeignKey('product.id'), nullable=False)
     start_date = Column(DateTime)
     end_date = Column(DateTime)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
 
     __table_args__ = (
         PrimaryKeyConstraint('account_id', 'product_id'),
