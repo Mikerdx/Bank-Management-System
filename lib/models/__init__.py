0ce33a9df07a7b2d3c2e1a75ba6398a2127b5f3d
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-28 23:06:55 +0300

    fixing init.py and adding notes

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -3,8 +3,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+#Tells SQLAlchemy to use an SQLite database file named bank.db in your project directory.
 DATABASE_URL = "sqlite:///bank.db"
 
+#Connection maker (creates a database engine ) and generate database sessions
 engine = create_engine(DATABASE_URL, echo=False, future=True)
 Session = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
@@ -17,12 +19,13 @@ from . import bank_branch
 from . import product
 from . import account_product
 
-
+#drops all tables and create them from scratch
 def reset_database():
     """Drop all tables and recreate them — use only during development."""
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
+#initialise database
 def init_database():
     """Create tables if they don't exist."""
     Base.metadata.create_all(engine)
