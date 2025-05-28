6e81c84db5afbd09862952f21a8ff172247f4ced
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-28 14:00:18 +0300

    create at

--- lib/models/account.py
+++ lib/models/account.py
@@ -1,5 +1,5 @@
 # lib/models/account.py
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from lib.models import Base
 
@@ -12,11 +12,13 @@ class Account(Base):
     balance = Column(Integer, default=0)
     profile_id = Column(Integer, ForeignKey('user_profile.id'), unique=True)
     branch_id = Column(Integer, ForeignKey('bank_branch.id'))
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
 
     profile = relationship("UserProfile", back_populates="account", uselist=False)
     branch = relationship("BankBranch", back_populates="accounts")
     transactions = relationship("Transaction", back_populates="account", cascade="all, delete-orphan")
-    account_products = relationship("AccountProduct", back_populates="account", cascade="all, delete-orphan")
     products = relationship(
         "Product",
         secondary="account_product",
