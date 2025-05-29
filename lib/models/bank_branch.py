a837b7e0613261a4fa47b2b33a03d7c361f727fa
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-29 12:24:41 +0300

    rship fix

--- lib/models/bank_branch.py
+++ lib/models/bank_branch.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import relationship
 from lib.models import Base
 
 class BankBranch(Base):
+#map the account model to the table in database
     __tablename__ = 'bank_branch'
-
+#column creation
     id = Column(Integer, primary_key=True)
     name = Column(String)
     location = Column(String)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-
+#R/ship of one to many
     accounts = relationship("Account", back_populates="branch")
