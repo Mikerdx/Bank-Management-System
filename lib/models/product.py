8b7adb53753f832224c22b1b5f95c7570d503b96
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-29 12:27:19 +0300

    product table

--- lib/models/product.py
+++ lib/models/product.py
@@ -4,14 +4,16 @@ from sqlalchemy.orm import relationship
 from lib.models import Base
 
 class Product(Base):
+#Map this model to the product table in  database.tells the name of table as in database
     __tablename__ = 'product'
-
+#column creation and timestamp addition
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(Text)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
+#R/ship (many to many)
     accounts = relationship(
         "Account",
         secondary="account_product",
